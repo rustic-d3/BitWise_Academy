3a841c5b5a00cf34b1575be9965212c1e4b36c75
Rustic Dumbravă <dumbravarustic454@gmail.com>
2026-05-04 21:32:56 +0300

    Solve some bugs and try to fetch data from backend on TeacherDashboard

--- frontend/src/pages/TeacherDashboard.tsx
+++ frontend/src/pages/TeacherDashboard.tsx
@@ -1,10 +1,27 @@
+import { useEffect, useState } from "react";
 import ClassSession from "../components/ClassSession";
 import InfoCard from "../components/InfoCard";
 import Navbar from "../components/Navbar";
 import { getUserRole } from "../helper-functions/DecodedToken";
+import api from "../api";
 
 export default function TeacherDashboard() {
   const role = getUserRole()?.toLowerCase() as "teacher";
+
+  const [teacherData, setTeacherData] = useState({});
+
+  useEffect(() => {
+    async function getTeacherData() {
+      const response = await api.get("api/teacher/profile");
+      if (response.statusText === "OK") {
+        setTeacherData(response.data);
+        console.log(response.data);
+      }
+    }
+
+    getTeacherData();
+  }, []);
+
   return (
     <div className="page-wrapper">
       <Navbar role="teacher" />
